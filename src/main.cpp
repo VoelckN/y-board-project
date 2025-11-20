553dc6e199bdc74f75dc376730e26e168edb9073
VoelckN <nick.voelckers@gmail.com>
2025-11-20 14:08:17 -0700

    Feat: Change color in x direction tilt

--- src/main.cpp
+++ src/main.cpp
@@ -10,28 +10,45 @@
 void setup() {
   Serial.begin(9600);
   Yboard.setup();
-  
-  //Add any code you just want to run once here.
 }
 
 void loop() {
-  // Add any code you want to run repeatedly here.
-  // Below are some examples of how to use different parts of the Y-Board.
-  // Delete anything you don't need, and add anything you want.
-  // For a full list of functions available on the Y-board, see the header file at .pio/libdeps/esp32/Y-Board v4/include/yboard.h from lines 39-330.
+  // For a full list of functions available on the Y-board, 
+  // see the header file at .pio/libdeps/esp32/Y-Board v4/include/yboard.h from lines 39-330.
   
+  /*
+  ***** INITIAL WORK *****
+  Get the device to change color with accelerometer data (in the x-direction)
+
+  */
+  if (Yboard.accelerometer_available()) {
+    accelerometer_data accel_data = Yboard.get_accelerometer();
+    x_value = accel_data.x;
+    y_value = accel_data.y;
+    z_value = accel_data.z;
+  }
+  Yboard.set_led_brightness(64);
+  if (x_value > 100) {
+    Yboard.set_all_leds_color(0, 255, 0);
+  }
+  else {
+    Yboard.set_all_leds_color(0,0,0);
+  }
+
   // ========LEDS========
-  Yboard.set_led_brightness(128);       // Set the brightness of all LEDs to 50% (128 out of 255)
-  Yboard.set_all_leds_color(0, 255, 0); // Set all LEDs to green
-  Yboard.set_led_color(1, 255, 0, 0);   // Set the first LED to red
+
+  // Yboard.set_led_brightness(128);       // Set the brightness of all LEDs to 50% (128 out of 255)
+  // Yboard.set_all_leds_color(0, 255, 0); // Set all LEDs to green
+  // Yboard.set_led_color(1, 255, 0, 0);   // Set the first LED to red
+
   // ====================
 
 
   // ========BUTTONS AND SWITCHES========
 
   // Use these commands to get values from the buttons and switches.
-  button_1 = Yboard.get_button(1);  
-  switch_1 = Yboard.get_switch(1);
+  // button_1 = Yboard.get_button(1);  
+  // switch_1 = Yboard.get_switch(1);
   // Then use those values however you need them
   // ====================================
   
@@ -41,7 +58,7 @@ void loop() {
   Yboard.display.setTextSize(1); // Set text size to 1 (smallest)
   Yboard.display.setTextColor(SSD1306_WHITE); // Set text color to white
   Yboard.display.setCursor(0, 0); // Set cursor to top-left corner
-  Yboard.display.println("Hello, Y-Board!"); // Print text to the display
+  Yboard.display.printf("x:%i", x_value); // Print text to the display
 
   // You can also draw shapes on the display. Here are some examples:
   Yboard.display.drawLine(0, 10, 127, 10, SSD1306_WHITE); // Draw a horizontal line across the display
@@ -61,12 +78,14 @@ void loop() {
   // Here's an example of how to get angle data from the accelerometer. The Values the accelerometer gives are between 0 and 999.
 
   // Use a block like this to get data from the accelerometer using the variables you declared. The if() makes sure you're only getting data when it's available.
-  if (Yboard.accelerometer_available()) {
-    accelerometer_data accel_data = Yboard.get_accelerometer();
-    x_value = accel_data.x;
-    y_value = accel_data.y;
-    z_value = accel_data.z;
-  }
+  
+  // if (Yboard.accelerometer_available()) {
+  //   accelerometer_data accel_data = Yboard.get_accelerometer();
+  //   x_value = accel_data.x;
+  //   y_value = accel_data.y;
+  //   z_value = accel_data.z;
+  // }
+  
   // Then you can use the x_value, y_value, and z_value variables to do whatever you want with the accelerometer data.
   // ============================
 }
